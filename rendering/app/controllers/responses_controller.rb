33a76812d8f13be95af2681683945961f3578127
sfespinosa <scottjames.espinosa@gmail.com>
2020-07-24 09:27:07 -0700

    Done.

--- rendering/app/controllers/responses_controller.rb
+++ rendering/app/controllers/responses_controller.rb
@@ -4,11 +4,11 @@ class ResponsesController < ApplicationController
   # for each of the controller actions,
   # render the data according to the name of the action
   def plain
-    data = "Early bird gets the worm. But cookie taste better than worm. So me sleep in. - Cookie Monster"
+    render plain: data = "Early bird gets the worm. But cookie taste better than worm. So me sleep in. - Cookie Monster"
   end
 
   def html
-    data = "<html>
+    render html: data = "<html>
               <body>
                 <q>Early bird gets the worm. But cookie taste better than worm. So me sleep in.</q>
                 <cite>Cookie Monster</cite>
@@ -17,31 +17,31 @@ class ResponsesController < ApplicationController
   end
 
   def json
-    data = {
+    render json: data = {
       author: "Cookie Monster",
       quote:"Early bird gets the worm. But cookie taste better than worm. So me sleep in.",
     }
   end
 
   def xml
-    data = "<quote>
+    render xml: data = "<quote>
     <author>Cookie Monster</author>
     <text>Early bird gets the worm. But cookie taste better than worm. So me sleep in.</text>
     </quote>"
   end
 
   def csv
-    data =  CSV.generate do |csv|
+    send_data (data =  CSV.generate do |csv|
       csv << ["author", "name"]
       csv << ["Cookie Monster", "Early bird gets the worm. But cookie taste better than worm. So me sleep in."]
-    end
+    end)
 
     # you'll need to use send_data instead of render
     # if you want the file to have a particular filename, pass the filename: parameter
   end
 
   def png
-    filename = "public/data.png"
+    send_file filename = "public/data.png"
 
     # you'll need to use send_file instead of render
     # if you want the image to show inline, use disposition: :inline
